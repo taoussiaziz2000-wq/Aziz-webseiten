d0a4ac6acc4c4c877d163a6a6a37f886613e74a9
githubaziz1973 <42500919+githubaziz1973@users.noreply.github.com>
2025-08-19 21:04:14 +0200

    file css and js aded to html file and function t o change color of my title and background color of body

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+let myTitle = document.getElementById("myHeadTitle")
+let changeColorOfmyTitle = function () {
+    let ChangeFontSize = function () {
 
+
+        if (document.body.style.backgroundColor = "white") {
+            myTitle.style.color = "orange"
+            myTitle.style.fontSize = "50px"
+            document.body.style.backgroundColor = "rgba(131, 169, 131, 1)"
+        }
+        else {
+            myTitle.style.color = "blue"
+        }
+    }
+
+    setTimeout(ChangeFontSize, 5000)
+}
+myTitle.onload = changeColorOfmyTitle();
